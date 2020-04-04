18ac1691fdfe1e80746e1cbcc703ecd90eb734ef
moaz <mohsin.afridi91@gmail.com>
2020-04-04 23:21:07 +0500

    'asd'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createContext } from "react";
+// To start  Context
+// Step: 1 => Create Context
+const MyContext = createContext();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Step: 2 => Create Provider Component
+// This is the place where data lives.
+class MyProvider extends Component {
+  state = {
+    name: "Mohsin Azam",
+    age: 28,
+    cool: true,
+  };
+  render() {
+    //const { value } = this.state;
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          incrementAge: () => this.setState({
+            age: this.state.age + 1
+          })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
+}
+
+class Family extends Component {
+  render() {
+    return (
+      <div>
+        <Person />
+      </div>
+    );
+  }
+}
+class Person extends Component {
+  render() {
+    return (
+      <MyContext.Consumer>
+        {(context) => (
+          <>
+            <p>I am Person App Component. Name : {context.state.name}</p>
+            <p>I am Person App Component. Age: {context.state.age}</p>
+
+            <button onClick={context.incrementAge}>+(update the context)</button>
+          </>
+        )}
+      </MyContext.Consumer>
+    );
+  }
 }
 
 export default App;
